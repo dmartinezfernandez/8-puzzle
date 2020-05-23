a975bd4edf1cecb840247d622ebe25989a536c56
dmartinezfernandez deloitte.es <dmartinezfernandez@deloitte.es>
2020-05-23 13:21:13 +0200

    vue-cli-plugin-vue-next

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 8-puzzle
 8-puzzle game for kids
+
+Vue 3 from scratch with no features:
+```
+vue create eight-puzzle
+vue add vue-next
+```
---
+++ eight-puzzle/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue'
+
+createApp({
+  render: function (h) { return h(App) },
+}).mount('#app')
